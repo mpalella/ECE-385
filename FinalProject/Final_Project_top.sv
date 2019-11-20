1d9af2c04d255a25814228453622f5c8fb44e0e7
Wong <zrwong2@illinois.edu>
2019-11-20 11:11:12 -0600

    testing w/ keyboard

--- FinalProject/Final_Project_top.sv
+++ FinalProject/Final_Project_top.sv
@@ -10,25 +10,25 @@ module Final_Project_top( input               CLOCK_50,
 //                                 VGA_BLANK_N,  //VGA Blank signal
 //                                 VGA_VS,       //VGA virtical sync signal
 //                                 VGA_HS,       //VGA horizontal sync signal
-//             // CY7C67200 Interface
-//             inout  wire  [15:0] OTG_DATA,     //CY7C67200 Data bus 16 Bits
-//             output logic [1:0]  OTG_ADDR,     //CY7C67200 Address 2 Bits
-//             output logic        OTG_CS_N,     //CY7C67200 Chip Select
-//                                 OTG_RD_N,     //CY7C67200 Write
-//                                 OTG_WR_N,     //CY7C67200 Read
-//                                 OTG_RST_N,    //CY7C67200 Reset
-//             input               OTG_INT,      //CY7C67200 Interrupt
-//             // SDRAM Interface for Nios II Software
-//             output logic [12:0] DRAM_ADDR,    //SDRAM Address 13 Bits
-//             inout  wire  [31:0] DRAM_DQ,      //SDRAM Data 32 Bits
-//             output logic [1:0]  DRAM_BA,      //SDRAM Bank Address 2 Bits
-//             output logic [3:0]  DRAM_DQM,     //SDRAM Data Mast 4 Bits
-//             output logic        DRAM_RAS_N,   //SDRAM Row Address Strobe
-//                                 DRAM_CAS_N,   //SDRAM Column Address Strobe
-//                                 DRAM_CKE,     //SDRAM Clock Enable
-//                                 DRAM_WE_N,    //SDRAM Write Enable
-//                                 DRAM_CS_N,    //SDRAM Chip Select
-//                                 DRAM_CLK,     //SDRAM Clock
+             // CY7C67200 Interface
+             inout  wire  [15:0] OTG_DATA,     //CY7C67200 Data bus 16 Bits
+             output logic [1:0]  OTG_ADDR,     //CY7C67200 Address 2 Bits
+             output logic        OTG_CS_N,     //CY7C67200 Chip Select
+                                 OTG_RD_N,     //CY7C67200 Write
+                                 OTG_WR_N,     //CY7C67200 Read
+                                 OTG_RST_N,    //CY7C67200 Reset
+             input               OTG_INT,      //CY7C67200 Interrupt
+             // SDRAM Interface for Nios II Software
+             output logic [12:0] DRAM_ADDR,    //SDRAM Address 13 Bits
+             inout  wire  [31:0] DRAM_DQ,      //SDRAM Data 32 Bits
+             output logic [1:0]  DRAM_BA,      //SDRAM Bank Address 2 Bits
+             output logic [3:0]  DRAM_DQM,     //SDRAM Data Mast 4 Bits
+             output logic        DRAM_RAS_N,   //SDRAM Row Address Strobe
+                                 DRAM_CAS_N,   //SDRAM Column Address Strobe
+                                 DRAM_CKE,     //SDRAM Clock Enable
+                                 DRAM_WE_N,    //SDRAM Write Enable
+                                 DRAM_CS_N,    //SDRAM Chip Select
+                                 DRAM_CLK,     //SDRAM Clock
 											
 				//DAC Interface
 				input logic 			AUD_BCLK,		//bit clock
@@ -36,14 +36,14 @@ module Final_Project_top( input               CLOCK_50,
 				output logic 			AUD_XCK,  		//master clock
 				output logic 			AUD_DACDAT,		//serial data
 				output logic 			I2C_SCLK,		//I2C clock
-				output logic 			I2C_SDAT,      //I2C serial data
+				output logic 			I2C_SDAT      //I2C serial data
 				
-				input logic [7:0] keycode
+				//input logic [7:0] keycode
 				
                     );
     
     logic Reset_h, Clk;
-    //logic [7:0] keycode;
+    logic [7:0] keycode;
 	 //logic [9:0] DrawX, DrawY;
 
     
@@ -52,54 +52,54 @@ module Final_Project_top( input               CLOCK_50,
         Reset_h <= ~(KEY[0]);        // The push buttons are active low
     end
     
-//    logic [1:0] hpi_addr;
-//    logic [15:0] hpi_data_in, hpi_data_out;
-//    logic hpi_r, hpi_w, hpi_cs, hpi_reset, freq_1;
+    logic [1:0] hpi_addr;
+    logic [15:0] hpi_data_in, hpi_data_out;
+    logic hpi_r, hpi_w, hpi_cs, hpi_reset, freq_1;
     
     // Interface between NIOS II and EZ-OTG chip
-//    hpi_io_intf hpi_io_inst(
-//                            .Clk(Clk),
-//                            .Reset(Reset_h),
-//                            // signals connected to NIOS II
-//                            .from_sw_address(hpi_addr),
-//                            .from_sw_data_in(hpi_data_in),
-//                            .from_sw_data_out(hpi_data_out),
-//                            .from_sw_r(hpi_r),
-//                            .from_sw_w(hpi_w),
-//                            .from_sw_cs(hpi_cs),
-//                            .from_sw_reset(hpi_reset),
-//                            // signals connected to EZ-OTG chip
-//                            .OTG_DATA(OTG_DATA),    
-//                            .OTG_ADDR(OTG_ADDR),    
-//                            .OTG_RD_N(OTG_RD_N),    
-//                            .OTG_WR_N(OTG_WR_N),    
-//                            .OTG_CS_N(OTG_CS_N),
-//                            .OTG_RST_N(OTG_RST_N)
-//    );
-//     
-//     // You need to make sure that the port names here match the ports in Qsys-generated codes.
-//     nios_system nios_system(
-//                             .clk_clk(Clk),         
-//                             .reset_reset_n(1'b1),    // Never reset NIOS
-//                             .sdram_wire_addr(DRAM_ADDR), 
-//                             .sdram_wire_ba(DRAM_BA),   
-//                             .sdram_wire_cas_n(DRAM_CAS_N),
-//                             .sdram_wire_cke(DRAM_CKE),  
-//                             .sdram_wire_cs_n(DRAM_CS_N), 
-//                             .sdram_wire_dq(DRAM_DQ),   
-//                             .sdram_wire_dqm(DRAM_DQM),  
-//                             .sdram_wire_ras_n(DRAM_RAS_N),
-//                             .sdram_wire_we_n(DRAM_WE_N), 
-//                             .sdram_clk_clk(DRAM_CLK),
-//                             .keyboard_export(keycode),  
-//                             .otg_hpi_addr_export(hpi_addr),
-//                             .otg_hpi_data_in_port(hpi_data_in),
-//                             .otg_hpi_data_out_port(hpi_data_out),
-//                             .otg_hpi_cs_export(hpi_cs),
-//                             .otg_hpi_read_export(hpi_r),
-//                             .otg_hpi_write_export(hpi_w),
-//                             .otg_hpi_reset_export(hpi_reset)
-//    );
+    hpi_io_intf hpi_io_inst(
+                            .Clk(Clk),
+                            .Reset(Reset_h),
+                            // signals connected to NIOS II
+                            .from_sw_address(hpi_addr),
+                            .from_sw_data_in(hpi_data_in),
+                            .from_sw_data_out(hpi_data_out),
+                            .from_sw_r(hpi_r),
+                            .from_sw_w(hpi_w),
+                            .from_sw_cs(hpi_cs),
+                            .from_sw_reset(hpi_reset),
+                            // signals connected to EZ-OTG chip
+                            .OTG_DATA(OTG_DATA),    
+                            .OTG_ADDR(OTG_ADDR),    
+                            .OTG_RD_N(OTG_RD_N),    
+                            .OTG_WR_N(OTG_WR_N),    
+                            .OTG_CS_N(OTG_CS_N),
+                            .OTG_RST_N(OTG_RST_N)
+    );
+     
+     // You need to make sure that the port names here match the ports in Qsys-generated codes.
+     nios_system nios_system(
+                             .clk_clk(Clk),         
+                             .reset_reset_n(1'b1),    // Never reset NIOS
+                             .sdram_wire_addr(DRAM_ADDR), 
+                             .sdram_wire_ba(DRAM_BA),   
+                             .sdram_wire_cas_n(DRAM_CAS_N),
+                             .sdram_wire_cke(DRAM_CKE),  
+                             .sdram_wire_cs_n(DRAM_CS_N), 
+                             .sdram_wire_dq(DRAM_DQ),   
+                             .sdram_wire_dqm(DRAM_DQM),  
+                             .sdram_wire_ras_n(DRAM_RAS_N),
+                             .sdram_wire_we_n(DRAM_WE_N), 
+                             .sdram_clk_clk(DRAM_CLK),
+                             .keyboard_export(keycode),  
+                             .otg_hpi_addr_export(hpi_addr),
+                             .otg_hpi_data_in_port(hpi_data_in),
+                             .otg_hpi_data_out_port(hpi_data_out),
+                             .otg_hpi_cs_export(hpi_cs),
+                             .otg_hpi_read_export(hpi_r),
+                             .otg_hpi_write_export(hpi_w),
+                             .otg_hpi_reset_export(hpi_reset)
+    );
 	 
 	 //synth logic variables
 	 logic init_done, init, chip_sel, sample_Clk, program_start, start, data_over;
