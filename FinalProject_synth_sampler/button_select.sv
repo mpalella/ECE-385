4fbcc433a79d30ce23bcf5812dad3a9a3313c30d
palella2 <palella2@illinois.edu>
2019-12-09 13:51:30 -0600

    fixed button select issues

--- FinalProject_synth_sampler/button_select.sv
+++ FinalProject_synth_sampler/button_select.sv
@@ -6,29 +6,85 @@ output logic [1:0] function_select
 );
 
 
-always_ff @ (posedge Clk)
-begin
+enum logic [3:0] {
 
-if(Reset)
-	function_select <= 2'd0;
-	
-else
-begin
+FUNCTION_0_LOW,
+FUNCTION_0_HIGH,
+FUNCTION_1_LOW,
+FUNCTION_1_HIGH,
+FUNCTION_2_LOW,
+FUNCTION_2_HIGH,
+FUNCTION_3_LOW,
+FUNCTION_3_HIGH,
+FUNCTION_4_LOW,
+FUNCTION_4_HIGH
 
-if(button)
 
-	function_select <= function_select + 2'b01;
-	
-
-
-if(function_select > 2'd4)
-	function_select <= 2'b0;
+}   curr_state, next_state;
 
 
 
-end
 
-end
+	//updates flip flop, current state is the only one
+    always_ff @ (posedge Clk)  
+    begin
+        if (Reset)
+            curr_state <= FUNCTION_0_LOW;
+        else 
+            curr_state <= next_state;
+    end
 
+	 
+	 always_comb
+    begin
+        
+			next_state  = curr_state;	
+			unique case (curr_state) 
+			
+			FUNCTION_0_LOW :	if(button)
+										next_state = FUNCTION_0_HIGH;
+			
+			FUNCTION_0_HIGH :	if(~button)
+										next_state = FUNCTION_1_LOW;
+										
+										
+			FUNCTION_1_LOW :	if(button)
+										next_state = FUNCTION_1_HIGH;
+			
+			FUNCTION_1_HIGH :	if(~button)
+										next_state = FUNCTION_2_LOW;
+										
+										
+										
+			FUNCTION_2_LOW :	if(button)
+										next_state = FUNCTION_2_HIGH;
+			
+			FUNCTION_2_HIGH :	if(~button)
+										next_state = FUNCTION_3_LOW;
+										
+										
+										
+			FUNCTION_3_LOW :	if(button)
+										next_state = FUNCTION_3_HIGH;
+			
+			FUNCTION_3_HIGH :	if(~button)
+										next_state = FUNCTION_0_LOW;
+			
+			
+			endcase
+			
+			case (curr_state)
+			
+				FUNCTION_0_LOW, FUNCTION_0_HIGH:  function_select = 2'd0;
+				
+				FUNCTION_1_LOW, FUNCTION_1_HIGH:  function_select = 2'd1;
+				
+				FUNCTION_2_LOW, FUNCTION_2_HIGH:  function_select = 2'd2;
+				
+				FUNCTION_3_LOW, FUNCTION_3_HIGH:  function_select = 2'd3;
+						
+			endcase
+			
+	end
 
 endmodule
